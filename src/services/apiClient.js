1c1838498e91ab9008453038255db609931452ac
Direct-Dev-Ru <info@direct-dev.ru>
2021-05-21 19:05:13 +0000

    json-server locally done

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   //   baseURL: config.apiBaseUrl,
-  baseURL: 'https://3000-silver-parakeet-uvgrqdoj.ws-eu04.gitpod.io',
+  baseURL: 'https://3000-silver-parakeet-uvgrqdoj.ws-eu07.gitpod.io',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
